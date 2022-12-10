f1757fef454edcba8ae6b8561a2d27639df9027c
JayBlankenship <40524287+JayBlankenship@users.noreply.github.com>
2022-12-09 21:11:46 -0600

    g

--- connect.php
+++ connect.php
@@ -1,3 +1,18 @@
 <?php
-    
+    $email = $_POST['email'];
+
+    //database connection
+    $conn = new sqli('localhost','root','','0_test');
+    if($conn->connection_error){
+        die('Connection Failed : '.$conn->connection_error);
+        echo "registration failed";
+    }else{
+    $stmt = $conn->prepare("insert into registration(email)
+            values(?)");
+        $stmt->bind_param("s",$email);
+        $stmt->execute();
+        echo "registration Successfully...";
+        $stmt->close();
+        $conn->close();
+    }
 ?>
