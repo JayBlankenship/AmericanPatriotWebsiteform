d60387abbd6977b12950f4ade3ae6155b5f4dc39
JayBlankenship <40524287+JayBlankenship@users.noreply.github.com>
2022-12-23 09:36:51 -0600

    dynamic html element working

--- demo/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ demo/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,14 +1,16 @@
 package com.example.demo
-
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
+import org.springframework.util.ResourceUtils.getFile
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.io.File
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
@@ -29,6 +31,7 @@ interface apwebsiteRepository: JpaRepository<emails,String>
 	@Query(value = "call InsertEmail(:Email)", nativeQuery = true)
 	fun InsertEmail(@Param("Email")Email: String):emails
 }
+@CrossOrigin
 @RestController
 @RequestMapping("api")
 class PlayerDataRestController(val apwebsiteRepo: apwebsiteRepository)
@@ -44,6 +47,13 @@ class PlayerDataRestController(val apwebsiteRepo: apwebsiteRepository)
 	{
 		println(Email.toString())
 		return apwebsiteRepo.InsertEmail(Email)
+	}
+	@GetMapping("GetFormone")
+	fun Getfirstform(): String
+	{
+		//http://localhost:8080/api/GetFormone
+		val file = File("C:\\Users\\jaybl\\Documents\\AmericanPatriotTest\\AmericanPatriotWebsiteform\\demo\\src\\main\\kotlin\\com\\example\\demo\\HTMLelement.txt")
+		return file.readText()
 	}
 }
 @Entity
